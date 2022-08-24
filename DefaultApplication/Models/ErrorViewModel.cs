914f0e2c2e98e9e43b71b5f62b625f6bec10bdda
Moshe Margareten <jewishspiderweb@gmail.com>
2022-08-24 12:52:54 -0400

    Add project files.

---
+++ DefaultApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DefaultApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
